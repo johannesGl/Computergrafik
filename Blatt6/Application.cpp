fa4d8330ec6d09a4ba289ef35672759da33f220f
JohannesGl <38466658+johannesGl@users.noreply.github.com>
2018-07-10 23:33:17 +0200

    New Version of Assignment 6 Solution

--- Blatt6/Application.cpp
+++ Blatt6/Application.cpp
@@ -14,10 +14,10 @@
 
 /*
 -----------------------------------------------------------------------------
-							Application.cpp
+Application.cpp
 -----------------------------------------------------------------------------
 
-This class is responsible for the rendering. 
+This class is responsible for the rendering.
 A short description of the methods:
 
 render()		-	Render loop method that is called for each frame.
@@ -39,16 +39,11 @@ float m_saturation;
 float m_brightness;
 float winkel = 0;
 bool changeOthografic = true;
-GLuint grass;
-GLuint stone;
-
-
-void loadTexture(char *fileName, GLuint *texId);
 
 /**
-	Constructor and destructor.
+Constructor and destructor.
 */
-Application::Application(int width, int height)	:
+Application::Application(int width, int height) :
 	m_width(width),
 	m_height(height)
 {
@@ -62,13 +57,14 @@ Application::~Application()
 
 
 /**
-	This method is called once. Use it to initialize shaders, vertex buffer objects, and elements that do not change during the render loop.
+This method is called once. Use it to initialize shaders, vertex buffer objects, and elements that do not change during the render loop.
 */
 void Application::init()
 {
 	// The shader constructor takes as argument the files in which the Vertex and the Fragment shader are defined.
-	
-	m_shader = new Shader("../Shader/Default33.vert.glsl", "../Shader/Default33.frag.glsl");
+
+	//m_shader = new Shader("../Shader/Default33.vert.glsl", "../Shader/Default33.frag.glsl");
+	m_shader = new Shader("../Shader/Texture.vert.glsl", "../Shader/Texture.frag.glsl");
 	//m_shader = new Shader("../Shader/Phong.vert.glsl", "../Shader/Phong.frag.glsl");
 
 	// --> If this is not working for you, use the line below.
@@ -79,9 +75,7 @@ void Application::init()
 	//m_vbo = ObjLoader::createVertexBufferObject("../Data/torus.obj");
 	//m_vbo = ObjLoader::createVertexBufferObject("../Data/box.obj");
 	m_vbo = ObjLoader::createVertexBufferObject("../Data/terrain.obj");
-
 	
-
 	loadTexture("../Data/grass.tga", &grass);
 	loadTexture("../Data/stone.tga", &stone);
 
@@ -93,13 +87,14 @@ void Application::init()
 
 
 /**
-	This method is called when the key 'F5' is pressed. It can be used to reload the shader if changes have been made, without having to restart the whole program.
+This method is called when the key 'F5' is pressed. It can be used to reload the shader if changes have been made, without having to restart the whole program.
 */
 void Application::reloadShader()
 {
 	delete m_shader;
 	//m_shader = new Shader("../Shader/Default33.vert.glsl", "../Shader/Default33.frag.glsl");
-	m_shader = new Shader("../Shader/Phong.vert.glsl", "../Shader/Phong.frag.glsl");
+	m_shader = new Shader("../Shader/Texture.vert.glsl", "../Shader/Texture.frag.glsl");
+	//m_shader = new Shader("../Shader/Phong.vert.glsl", "../Shader/Phong.frag.glsl");
 	// --> If this is not working for you, use the line below.
 	//m_shader = new Shader("../Shader/Default31.vert.glsl", "../Shader/Default31.frag.glsl");
 }
@@ -107,7 +102,7 @@ void Application::reloadShader()
 
 
 /**
-	This method is called when a key is pressed / released. Use it to bind actions to keys.
+This method is called when a key is pressed / released. Use it to bind actions to keys.
 */
 void Application::onKey(GLint key, GLint scancode, GLint action, GLint mods) {
 
@@ -149,7 +144,7 @@ void Application::onKey(GLint key, GLint scancode, GLint action, GLint mods) {
 		// key '3' decreases the saturation value by x
 		if (key == '3' && action == GLFW_PRESS && m_saturation >= 0.05)
 		{
-			m_saturation -= 0.05; 
+			m_saturation -= 0.05;
 		}
 
 		// key '4' increases the saturation value by x
@@ -178,47 +173,33 @@ void Application::onKey(GLint key, GLint scancode, GLint action, GLint mods) {
 	}
 }
 
-
-void loadTexture(char *fileName, GLuint *texId)
+/**
+Is called to load the taxture by name and id
+*/
+void Application::loadTexture(char *fileName, GLuint *textureId)
 {
-	glGenTextures(1, texId);
-	glBindTexture(GL_TEXTURE_2D, *texId);
-	
+	TgaLoader loader;
+	Texture texture;
+
+	loader.loadTga(texture, fileName);
+
+	glGenTextures(1, textureId);
+	glBindTexture(GL_TEXTURE_2D, *textureId);
+
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	TgaLoader loader;
-	Texture tex; 
-	
-	loader.loadTga(tex, fileName);
-
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, (GLuint)tex.width, (GLuint)tex.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, tex.imageData);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, (GLuint)texture.width, (GLuint)texture.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture.imageData);
 	glEnable(GL_TEXTURE_2D);
 	glGenerateMipmap(GL_TEXTURE_2D);
-
-	
-	
-	/*
-	for (int i = sizeof(tex.imageData) - 1; i >= 0; i--)
-	{
-		cout << tex.imageData[i] + " " << std::endl;
-	}
-	*/
-
-	//std::cout << tex.bpp << std::endl;
-
-	//std::cout << tex.width << std::endl;
-	//std::cout << "success";
 }
 
-
 /**
-	The render loop. This is called for each frame.
+The render loop. This is called for each frame.
 */
 void Application::render()
 {
-    
+
 	// Setting up the viewport of the window.
 	glViewport(0, 0, m_framebufferWidth, m_framebufferHeight);
 
@@ -249,15 +230,11 @@ void Application::render()
 	float n = 0.1f;
 	float fov = 45.0f;
 	float zoom = tanf(fov * math_radians / 2.0f);
-	mat4 orthographic =    mat4((float)m_width / (float) m_height, 0.0f, 0.0f, 0.0f,
-								0.0f, (float)m_width/(float)m_height, 0.0f, 0.0f,
-								0.0f, 0.0f, (-2)/(f-n), -(n + f) / (f - n),
-							    0.0f, 0.0f, 0.0f, 1.0f)	;
-	//mat4 orthographic = projection * mat4(1/n,0,0,0, 0,1/n,0,0, 0,0,0,-((n-f)/f*(f-n)*n), 0,0,-1,(((-f-n)*(n-f))/(2*f*(f-n)*n)) + ((f+n)/(2*f*n)));
-	//mat4 orthographic = projection * model;
-	
-	
 
+	mat4 orthographic = mat4((float)m_width / (float)m_height, 0.0f, 0.0f, 0.0f,
+		0.0f, (float)m_width / (float)m_height, 0.0f, 0.0f,
+		0.0f, 0.0f, (-2) / (f - n), -(n + f) / (f - n),
+		0.0f, 0.0f, 0.0f, 1.0f);	
 
 	winkel += 0.001f;
 	//std::cout << winkel;
@@ -265,82 +242,73 @@ void Application::render()
 	{
 		winkel = 0;
 	}
-
 	
 
 	// The shader is activated. The upcoming geometry will be passed through the Vertex and Fragment shaders.
 	m_shader->bind();
 
-		// Parameters can be transferred to the GPU as "uniform" variables (visible in all shaders).
-		// Other types (similar for int):
-		//	setf	->	1D float
-		//	set2f	->	2D float
-		//	set3f	->	3D float
-		if (changeOthografic)
-		{
-			m_shader->setMatrix("matProjection", projection, GL_TRUE);
-			m_shader->setMatrix("matOrtographic", mat4::identitiy(), GL_TRUE);
-			m_shader->seti("flag", 0);
-		}
-		else
-		{
-			m_shader->setMatrix("matOrtographic", orthographic, GL_TRUE);
-			m_shader->setMatrix("matProjection", mat4::identitiy(), GL_TRUE);
-			m_shader->seti("flag", 1);
-		}
+	// Parameters can be transferred to the GPU as "uniform" variables (visible in all shaders).
+	// Other types (similar for int):
+	//	setf	->	1D float
+	//	set2f	->	2D float
+	//	set3f	->	3D float
+
+	if (changeOthografic)
+	{
+		m_shader->setMatrix("matProjection", projection, GL_TRUE);
+		m_shader->setMatrix("matOrtographic", mat4::identitiy(), GL_TRUE);
+		m_shader->seti("flag", 0);
+	}
+	else
+	{
+		m_shader->setMatrix("matOrtographic", orthographic, GL_TRUE);
+		m_shader->setMatrix("matProjection", mat4::identitiy(), GL_TRUE);
+		m_shader->seti("flag", 1);
+	}
+
+	m_shader->setf("winkel", winkel);
+	m_shader->setMatrix("matView", view, GL_TRUE);
+	m_shader->setMatrix("matModel", model, GL_TRUE);
+	m_shader->set4f("camPos", inf.a14, inf.a24, inf.a34, inf.a44);
+
+	vec3 hsv_offsets;
+	hsv_offsets.x = m_hue;
+	hsv_offsets.y = m_saturation;
+	hsv_offsets.z = m_brightness;
+
+
+	m_shader->set3f("hsv_user_input", hsv_offsets);
+
+	m_shininess = 0.5f;
+
+	vec3 lightPos = vec3(250.0f, 250.0f, 2.0f);
+	vec3 lightCol = vec3(1.0f, 1.0f, 2.0f);
+	vec3 diffuse = vec3(0.0f, 0.0f, 0.0f);
+	vec3 specular = vec3(1 - diffuse.x, 1 - diffuse.y, 1 - diffuse.z);
+
+	m_ambient = lightCol * 0.0f;
+
+
+	m_shader->setf("shininess", m_shininess);
+	m_shader->set3f("Ambient", m_ambient);
+
+	m_shader->set3f("Diffuse", diffuse);
+	m_shader->set3f("Specular", specular);
+	m_shader->set3f("LightPos", lightPos);
+	m_shader->set3f("LightColor", lightCol);
+
+	//new
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, grass);
+	m_shader->seti("tex1", 1);
+
+
+	glActiveTexture(GL_TEXTURE2);
+	glBindTexture(GL_TEXTURE_2D, stone);
+	m_shader->seti("tex2", 2);
 	
-		m_shader->setf("winkel",winkel);
-		m_shader->setMatrix("matView", view, GL_TRUE);
-		m_shader->setMatrix("matModel", model, GL_TRUE);
-		m_shader->set4f("camPos", inf.a14, inf.a24, inf.a34, inf.a44);
-
-		vec3 hsv_offsets;
-		hsv_offsets.x = m_hue;
-		hsv_offsets.y = m_saturation;
-		hsv_offsets.z = m_brightness;
-
-		
-
-		//std::cout << "* Debug " << m_hue << std::endl;
-
-		m_shader->set3f("hsv_user_input", hsv_offsets);
-
-		m_shininess = 0.5f;
-
-		vec3 lightPos = vec3(250.0f, 250.0f, 2.0f);
-		vec3 lightCol = vec3(1.0f, 1.0f, 2.0f);
-		vec3 diffuse = vec3(0.0f, 0.0f, 0.0f);
-		vec3 specular = vec3(1-diffuse.x, 1 - diffuse.y, 1 - diffuse.z);
-
-		m_ambient = lightCol * 0.0f;
-	
-
-		m_shader->setf("shininess", m_shininess);
-		m_shader->set3f("Ambient", m_ambient);
-
-		m_shader->set3f("Diffuse", diffuse);
-		m_shader->set3f("Specular", specular);
-		m_shader->set3f("LightPos", lightPos);
-		m_shader->set3f("LightColor", lightCol);
-
-		//glBindTexture(GL_TEXTURE_2D, *texId);
-
-		glActiveTexture(GL_TEXTURE1); 
-		glBindTexture(GL_TEXTURE_2D, grass); 
-		m_shader->seti("tex1", 1);
-
-		
-		glActiveTexture(GL_TEXTURE2); 
-		glBindTexture(GL_TEXTURE_2D, stone); 
-		m_shader->seti("tex2", 2);
-		
-
-		//m_hue = 0;
-		//m_saturation = 0;
-		//m_brightness = 0;
-
-		// The geometry is pushed through the rendering pipeline. Vertex shader is called for each vertex; fragment shader for each fragment (potential pixel).
-		m_vbo->render();
+	// The geometry is pushed through the rendering pipeline. Vertex shader is called for each vertex; fragment shader for each fragment (potential pixel).
+	m_vbo->render();
 
 	// The shader is deactivated.
 	m_shader->release();
